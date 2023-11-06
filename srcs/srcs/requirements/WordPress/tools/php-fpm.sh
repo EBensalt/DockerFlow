e3a4ef7170cbd100a87526035ed31242e31bdf68
Elmehdi Bensaltana <ebensalt@c4r5p5.1337.ma>
2023-11-06 21:22:50 +0100

    miss

--- srcs/srcs/requirements/WordPress/tools/php-fpm.sh
+++ srcs/srcs/requirements/WordPress/tools/php-fpm.sh
@@ -12,9 +12,9 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 sed -i "s/database_name_here/wordpress/; s/username_here/$MARIADB_USER/; s/password_here/$MARIADB_USER_PASSWORD/; s/localhost/mariadb_container/" /var/www/html/wordpress/wp-config.php
+sleep 1
 wp core install --url="ebensalt.42.fr" --title="Inception" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL" --allow-root --path="/var/www/html/wordpress"
 wp user create $WORDPRESS_TEST_USER $WORDPRESS_TEST_EMAIL --user_pass="$WORDPRESS_TEST_PASSWORD" --allow-root --path="/var/www/html/wordpress"
 fi
-sleep 1
 service php7.4-fpm stop
 exec php-fpm7.4 -F
